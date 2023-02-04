11171a9ff31899e95b2aa5f6888cd016934f0a0c
RadimYassin <radimyassin20@gamil.com>
2023-02-04 23:46:16 +0100

    some changes

--- src/Component/pages/Market.js
+++ src/Component/pages/Market.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { client } from '../../axios'
 import Product from './Product'
-const Market = ({data }) => {
+const Market = ({data ,search,setSearch,setData}) => {
 
-
-
+  useEffect(() => {
+   
+    client.get(`/search?q=${search}`).then((res) => setData(res.data.products))
+}, [search])
   return (
     <div>
+      search :
+        <input value={search} onChange={e=>setSearch(e.target.value)} type='text'/>
       {data.map((item,index) => {
 
 
