771151307df60f6d9c6949da11034e54cb6eb2c5
RadimYassin <radimyassin20@gamil.com>
2023-02-04 23:52:41 +0100

    some changes

--- src/Component/pages/PageError.js
+++ src/Component/pages/PageError.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function PageError() {
   return (
     <div>
-      
+      <h1>error 404 !</h1>
+    <Link to='/'>store</Link>
     </div>
   )
 }
