c727d368b1f8f364bd93affe6cf7b639ef81e950
RadimYassin <radimyassin20@gamil.com>
2023-02-06 23:28:51 +0100

    add 3 case

--- src/redux/reducers/AppReducer.js
+++ src/redux/reducers/AppReducer.js
@@ -6,15 +6,28 @@ let init={
 
 function AppReducer (state=init,{type,payload}){
     switch (type) {
-        case "ADD":
-          return {...state,Cart:[...state.Cart,payload]}
+          case "ADD":
+               return {...state,Cart:[...state.Cart,payload]}
           case "Del_from_cart":
-            return {...state,Cart:state.Cart.filter((item)=>item.id!==payload)}
+                return {...state,Cart:state.Cart.filter((item)=>item.id!==payload),total:state.total.filter((item)=>item.id!==payload)}
            
           case 'ADD_PRICE':
-               return {...state,total:[...state.total,payload]}
-          case 'Del_PRICE':
-                return {...state,total:state.total.filter((item)=>item!==payload)}
+                 return {...state,total:[...state.total,payload]}
+          case 'ADD_Q':
+                  return {...state,total:state.total.map(i=>{
+                        if (i.id==payload) {
+                            return {...i,q:i.q+1}
+                        }
+                        return i
+                      })}
+          case 'DEL_Q':
+                  return {...state,total:state.total.map(i=>{
+                    if (i.id==payload) {
+                        return {...i,q:i.q-1}
+                    }
+                   return i
+                  })}
+           
         default: return state ;
            
     }
